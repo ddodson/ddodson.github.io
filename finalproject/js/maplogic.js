4f2014a676377683bc3acd26d7659f07b5438924
David Dodson <ddodson@nextworth.com>
2018-07-17 15:28:49 -0400

    Update maplogic.js

--- finalproject/js/maplogic.js
+++ finalproject/js/maplogic.js
@@ -11,110 +11,92 @@
  * With more time I would move this info to a database and do an AJAX.
  * 
  * @type Array
+ * 
+ * @author David Dodson <david.dodson.001@gmail.com>
  */
-var tour_stop_data = [
-    {
-      date: new Date(2018, 07, 03, 00, 00, 00, 00),
-      lat: 39.9831403,
-      lng: -83.2710211,
-      city: 'Columbus',
-      state: 'OH',
-      venue: {},
-      fb: '',
-      performers: ['Tactil Vision', 'Freeze Etch', 'Architect', 'Textbeak'],
-      flier: 'images/0803.jpg',
+var tour_stop_data = [{
+    date: new Date(2018, 07, 03, 00, 00, 00, 00),
+    lat: 39.9831403,
+    lng: -83.2710211,
+    city: 'Columbus',
+    state: 'OH',
+    venue: {},
+    fb: '',
+    performers: ['Tactil Vision', 'Freeze Etch', 'Architect', 'Textbeak'],
+    flier: 'images/0803.jpg',
+  },{
+    date: new Date(2018, 07, 04, 00, 00, 00, 00),
+    lat: 41.4977042,
+    lng: -81.8459456,
+    city: 'Cleveland',
+    state: 'OH',
+    venue: {},
+    fb: '',
+    performers: ['Abstinence','Nau-Zee-auN','Tactil Vision','Red Pyramid','Textbeak'],
+    flier: 'images/default.jpg',
+  },{
+    date: new Date(2018, 07, 05, 00, 00, 00, 00),
+    lat: 42.348941,
+    lng: -83.0644807,
+    city: 'Detroit',
+    state: 'MI',
+    venue: {
+      name: 'The Old Miami',
+      address: '3930 Cass Ave, Detroit, Michigan 48201',
+      phone: '(313) 831-3830',
     },
-    {
-      date: new Date(2018, 07, 04, 00, 00, 00, 00),
-      lat: 41.4977042,
-      lng: -81.8459456,
-      city: 'Cleveland',
-      state: 'OH',
-      venue: {},
-      fb: '',
-      performers: ['Abstinence','Nau-Zee-auN','Tactil Vision','Red Pyramid','Textbeak'],
-      flier: 'images/default.jpg',
+    fb: '',
+    performers: ['Abstinence','Nau-Zee-auN','Journey to the Center of the Colon','Freeze Etch','Fluxion A/D','S.A.P.','DJ Nemesis'],
+    flier: 'images/0805.jpg',
+  },{
+    date: new Date(2018, 07, 11, 00, 00, 00, 00),
+    lat: 39.1218698,
+    lng: -84.5334866,
+    city: 'Cincinnati',
+    state: 'OH',
+    venue: {
+      name: 'The Mockbee',
+      address: '2260 Central Pkwy, Cincinnati, OH 45214',
+      phone: '(513) 621-1900',
     },
-    {
-      date: new Date(2018, 07, 05, 00, 00, 00, 00),
-      lat: 42.348941,
-      lng: -83.0644807,
-      city: 'Detroit',
-      state: 'MI',
-      venue: {
-        name: 'The Old Miami',
-        address: '3930 Cass Ave, Detroit, Michigan 48201',
-        phone: '(313) 831-3830',
-      },
-      fb: '',
-      performers: ['Abstinence','Nau-Zee-auN','Journey to the Center of the Colon','Freeze Etch','Fluxion A/D','S.A.P.','DJ Nemesis'],
-      flier: 'images/0805.jpg',
+    fb: 'https://www.facebook.com/events/394109257765788/',
+    performers: ['Abstinence','Nau-Zee-auN','Journey to the Center of the Colon','Tactil Vision','Freeze Etch','Decide Today','Divtech'],
+    flier: 'images/0811.jpg',
+  },{
+    date: new Date(2018, 07, 12, 00, 00, 00, 00),
+    lat: 40.008639,
+    lng: -83.0002007,
+    city: 'Columbus',
+    state: 'OH',
+    venue: {
+      name: 'Cafe Bourbon Street',
+      address: '2216 Summit St, Columbus, OH 43201',
+      phone: '(614) 725-5256',
+      website: 'cafebourbonstreet.com',
     },
-/*
-    //unconfirmed date to uncomment if secured
-    {
-      date: new Date(2018, 07, 07, 00, 00, 00, 00),
-      lat: 42.3528795,
-      lng: -83.2392906,
-      city: 'Detroit',
-      state: 'MI',
-      venue: {
-        name: 'Detroit Industrial Underground promo spot',
-        address: 'tba',
-        phone: '',
-      },
-      fb: '',
-      performers: ['Abstinence','Nau-Zee-auN'],
-      flier: 'images/default.jpg',
-    }, */
-    {
-      date: new Date(2018, 07, 11, 00, 00, 00, 00),
-      lat: 39.1218698,
-      lng: -84.5334866,
-      city: 'Cincinnati',
-      state: 'OH',
-      venue: {
-        name: 'The Mockbee',
-        address: '2260 Central Pkwy, Cincinnati, OH 45214',
-        phone: '(513) 621-1900',
-      },
-      fb: 'https://www.facebook.com/events/394109257765788/',
-      performers: ['Abstinence','Nau-Zee-auN','Journey to the Center of the Colon','Tactil Vision','Freeze Etch','Decide Today','Divtech'],
-      flier: 'images/0811.jpg',
+    fb: 'https://www.facebook.com/events/353554445173582/',
+    performers: ['Abstinence','Nau-Zee-auN','Tactil Vision','Red Pyramid','Curse of Cassandra'],
+    flier: 'images/0812.jpg',
+  },{
+    date: new Date(2018, 07, 14, 00, 00, 00, 00),
+    lat: 42.1232118,
+    lng: -80.0849867,
+    city: 'Erie',
+    state: 'PA',
+    venue: {
+      name: 'Basement Transmissions Concert Hall',
+      address: '145 W 11th St, Erie, PA 16501',
+      phone: '(814) 520-8328',
     },
-    {
-      date: new Date(2018, 07, 12, 00, 00, 00, 00),
-      lat: 40.008639,
-      lng: -83.0002007,
-      city: 'Columbus',
-      state: 'OH',
-      venue: {
-        name: 'Cafe Bourbon Street',
-        address: '2216 Summit St, Columbus, OH 43201',
-        phone: '(614) 725-5256',
-        website: 'cafebourbonstreet.com',
-      },
-      fb: 'https://www.facebook.com/events/353554445173582/',
-      performers: ['Abstinence','Nau-Zee-auN','Tactil Vision','Red Pyramid','Curse of Cassandra'],
-      flier: 'images/0812.jpg',
-    },
-    {
-      date: new Date(2018, 07, 14, 00, 00, 00, 00),
-      lat: 42.1232118,
-      lng: -80.0849867,
-      city: 'Erie',
-      state: 'PA',
-      venue: {
-        name: 'Basement Transmissions Concert Hall',
-        address: '145 W 11th St, Erie, PA 16501',
-        phone: '(814) 520-8328',
-      },
-      fb: 'https://www.facebook.com/events/394109257765788/',
-      performers: ['Abstinence','Nau-Zee-auN','Journey to the Center of the Colon','Tactil Vision','Greyscape'],
-      flier: 'images/default.jpg',
-    }
-];
+    fb: 'https://www.facebook.com/events/394109257765788/',
+    performers: ['Abstinence','Nau-Zee-auN','Journey to the Center of the Colon','Tactil Vision','Greyscape'],
+    flier: 'images/default.jpg',
+  }];
 
+/**
+ * myMap build's the map
+ * @author David Dodson <david.dodson.001@gmail.com>
+ */
 function myMap() {
   var mapCanvas = document.getElementById("map");
 
@@ -207,7 +189,8 @@ function myMap() {
   var markers = [];
   var coordinates = [];
   var contentString = [];
-  var infowindow = [];
+  //var infowindow = [];
+  var infoWindow = new google.maps.InfoWindow(), marker, i;
 
   for (var i = 0; i < tour_stop_data.length; i++) {
     coordinates[i] = { lat: tour_stop_data[i].lat, lng: tour_stop_data[i].lng };
@@ -229,9 +212,12 @@ function myMap() {
       '<p>address: '+tour_stop_data[i].venue.address+'</p>'
       '<div id="bodyContent"><p>line up: '+lineup+'</p><img src="'+tour_stop_data[i].flier+"'></div></div>";
 
+/*
     infowindow[i] = new google.maps.InfoWindow({
       content: contentString[i],
     });
+*/
+    infoWindow[i] = new google.maps.InfoWindow(), marker, i;
 
     markers[i] = new google.maps.Marker({
       animation: google.maps.Animation.DROP,
@@ -242,10 +228,20 @@ function myMap() {
     });
 
     markers[i].setMap(map);
-    markers[i].addListener('click', function() {
+/*    markers[i].addListener('click', function() {
       var _i = i;
       infowindow[_i].open(map, markers[_i]);
     });
+*/
+
+    // Allow each marker to have an info window    
+    google.maps.event.addListener(markers[i], 'click', (function(marker, i, contentString) {
+        return function() {
+            infoWindow[i].setContent(contentString);
+            infoWindow[i].open(map, marker);
+        }
+    })(markers[i], i, contentString[i]));
+
   }//end of for loop
 
   //create travel path and animated arrow
@@ -271,55 +267,23 @@ function myMap() {
     strokeWeight: 5,
   });
 
-//  path.setMap(map);
   line.setMap(map);
 
   animate(line);
-
-/*  var contentString = '<div id="content" style="background-color:#000;">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString,
-  });
-
-  var marker = new google.maps.Marker({
-    position: {lat: 41, lng: -80.0849867},
-    map: map,
-    title: 'Uluru (Ayers Rock)'
-  });
-
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
-*/
 }
 
+/**
+ * animate Makes the arrow travel from show to show in chronological order on a loop.
+ * @param  line Google API polyline object
+ * @author David Dodson <david.dodson.001@gmail.com>
+ */
 function animate(line) {
-    var count = 0;
+    var x = 0;
     window.setInterval(function() {
-      count = (count + 1) % 200;
+      x = (x + 1) % 200;
 
       var icons = line.get('icons');
-      icons[0].offset = (count / 2) + '%';
+      icons[0].offset = (x / 2) + '%';
       line.set('icons', icons);
   }, 30);
 }
